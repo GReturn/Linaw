45bf0325abf1bf29e35dd9c6eaa6d10664a9a880
YelzyDy <zendymariel.dy@gmail.com>
2026-03-01 16:56:59 +0800

    Feat: login/register with db

--- linaw-app/src/main.jsx
+++ linaw-app/src/main.jsx
@@ -4,12 +4,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import InteractiveReader from './components/Notebook';
 import './index.css';
+import AuthPage from "./pages/AuthPage";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<AuthPage />} />
         <Route path="/notebook/:id" element={<InteractiveReader />} />
       </Routes>
     </BrowserRouter>
