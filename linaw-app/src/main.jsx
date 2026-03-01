aa0c49393b3a2c67015f59db6150492351aead3c
florenziooo <gilflorenz.sastre@cit.edu>
2026-03-01 15:34:11 +0800

    fix: routed properly by notebook id

--- linaw-app/src/main.jsx
+++ linaw-app/src/main.jsx
@@ -1,11 +1,17 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import InteractiveReader from './components/Notebook';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/notebook/:id" element={<InteractiveReader />} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
 );
