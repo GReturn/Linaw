893ecc8253e271ce61fa2fa22ed5127c8e931857
florenziooo <gilflorenz.sastre@cit.edu>
2026-03-02 00:40:12 +0800

    fix: attempt reroute endpoint

--- linaw-app/src/App.jsx
+++ linaw-app/src/App.jsx
@@ -2,12 +2,21 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import InteractiveReader from './components/Notebook'
 import AuthPage from "./pages/AuthPage";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/notebook/:id" element={<InteractiveReader />} />
+      <Route path="/" element={
+        <ProtectedRoute>
+          <Home />
+        </ProtectedRoute>
+      } />
+      <Route path="/notebook/:id" element={
+        <ProtectedRoute>
+          <InteractiveReader />
+        </ProtectedRoute>
+      } />
       <Route path="/auth" element={<AuthPage />} />
     </Routes>
   );
--- linaw-app/src/main.jsx
+++ linaw-app/src/main.jsx
@@ -1,19 +1,30 @@
-    import React from 'react';
-    import ReactDOM from 'react-dom/client';
-    import { BrowserRouter, Routes, Route } from 'react-router-dom';
-    import Home from './pages/Home';
-    import InteractiveReader from './components/Notebook';
-    import './index.css';
-    import AuthPage from "./pages/AuthPage";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { AuthProvider } from './context/AuthContext';
+import ProtectedRoute from './components/ProtectedRoute';
+import Home from './pages/Home';
+import InteractiveReader from './components/Notebook';
+import './index.css';
+import AuthPage from "./pages/AuthPage";
 
-    ReactDOM.createRoot(document.getElementById('root')).render(
-      <React.StrictMode>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/notebook/:id" element={<InteractiveReader />} />
-            <Route path="/auth" element={<AuthPage />} />
-          </Routes>
-        </BrowserRouter>
-      </React.StrictMode>
-    );
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/notebook/:id"
+            element={
+              <ProtectedRoute>
+                <InteractiveReader />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
+);
